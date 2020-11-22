2ad98f457a9619d7d4e7554cb3d1565134869eb8
tianyahechy <tianyahechu2004@163.com>
2020-11-22 15:01:43 +0800

    CVideoSoftware.h

--- irrlicht/CVideoSoftware.h
+++ irrlicht/CVideoSoftware.h
@@ -30,7 +30,29 @@ namespace irr
 				const core::position2d<s32>& destPos,
 				const core::rectEx<s32>& sourceRect,
 				const core::rectEx<s32>* clipRect = 0,
-				Color color = )
+				Color color = Color(255, 255, 255, 255),
+				bool useAlphaChannelOfTexture = false);
+			virtual void draw2DRectangle(Color color, const core::rect<s32>& pos, const core::rectEx<s32>* clip = 0);
+
+		protected:
+			void setRenderTarget(video::ISurface* surface);
+			void setTexture(video::ITexture* texture);
+			video::ISurface* _backBuffer;
+			video::ISurfacePresenter* _presenter;
+			void switchToTriangleRenderer(ETriangleRenderer renderer);
+			void selectRightTriangeRenderer();
+			core::array<S2DVertex> _transformedPoints;
+			video::ITexture* _renderTargetTexture;
+			video::ISurface* _renderTargetSurface;
+			core::position2d<s32> _renderTargetSize;
+			core::dimension2d<s32> _viewportSize;
+			core::matrix4 _transformationMatrix[TS_COUNT];
+			IK3DtriangleRenderer* _currentTriangleRenderer;
+			IK3DtriangleRenderer* _trangleRenderers[ETR_COUNT];
+			ETriangleRenderer _currentRenderer;
+			IZBuffer* _zBuffer;
+			video::ITexture* _texture;
+			SMaterial _material
 
 
 		};
